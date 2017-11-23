5f29ae761cc2dab46464c12b01de60638c830a19
chloe <chloevergreen@gmail.com>
2017-11-23 22:13:08 +0900

    memberDto 작성 완료

--- src/com/april/member/memberDto.java
+++ src/com/april/member/memberDto.java
@@ -1,11 +1,58 @@
 package com.april.member;
 
-//import 식별화
+import java.io.Serializable;
 
 public class memberDto {
-	//id, 이름, 비번, 이메일, 계급
+	private String id;
+	private String name;
+	private String pw;
+	private String email;
+	private int authority;
 	
-	//print를 overide해서 
+	public memberDto(){}
 	
-	//본격 실습 해볼까? from chloe
+	public memberDto(String id, String name, String pw, String email, int authority) {
+		this.id = id;
+		this.name = name;
+		this.pw = pw;
+		this.email = email;
+		this.authority = authority;
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getAuthority() {
+		return authority;
+	}
+	public void setAuthority(int authority) {
+		this.authority = authority;
+	}
+
+	@Override
+	public String toString() {
+		return "memberDto [id=" + id + ", name=" + name + ", pw=" + pw + ", email=" + email + ", authority=" + authority
+				+ "]";
+	}
 }
