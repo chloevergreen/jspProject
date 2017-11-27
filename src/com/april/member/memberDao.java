988f510b7e6ebdba367fb66f62e48c2074e44cc1
chloe <chloevergreen@gmail.com>
2017-11-27 21:51:05 +0900

    dto private 변수 접미사_ 붙이기 dao 생성자, 로그, 싱글톤 작성

--- src/com/april/member/memberDao.java
+++ src/com/april/member/memberDao.java
@@ -2,20 +2,34 @@ package com.april.member;
 
 public class memberDao implements imemberDao {
 	private boolean isSuccess = false;
-	private memberDao memberDao;
+	private static memberDao memberDao;
 	
-	//생성자에 ojdbc
+	private memberDao(){
+		try {
+			Class.forName("oracle.jdbc.diver.OracleDriver");
+			log("1/6 Success");
+		} catch (ClassNotFoundException e) {
+			log("1/6 Fail", e);
+			System.out.println(e.getMessage());
+		}
+	}
 	
-	//상굹톤 dao
+	public static memberDao getInstance(){
+		if(memberDao == null){
+			memberDao = new memberDao();
+		}
+		return memberDao;
+	}
 	
 	//디비 연결 메소드
 	
+	//디비 연결 종료하는 메서드
+	
 	//디비 닫는 메소드
 	
-	//추가 멤버 오버라이드
-	//sql작성
 	@Override
 	public boolean addMember(memberDto member) {
+		//sql작성
 		return false;
 	}
 
@@ -24,5 +38,15 @@ public class memberDao implements imemberDao {
 		return null;
 	}
 	
-	//로그
+	public void log(String msg){
+		if(isSuccess){
+			System.out.println(getClass() + " : " + msg);
+		}
+	}
+	public void log(String msg, Exception e){
+		//isSuccess 를 true로 따로 대입 해야하는가...?
+		if(isSuccess){
+			System.out.println(e + " : " + getClass() + " : " + msg);
+		}
+	}
 }
--- src/com/april/member/memberDto.java
+++ src/com/april/member/memberDto.java
@@ -5,56 +5,56 @@ import java.io.Serializable;
 public class memberDto {
 	private static final long serialVersionUID = 1L;
 	
-	private String id;
-	private String name;
-	private transient String pw;
-	private String email;
-	private int authority;
+	private String id_;
+	private String name_;
+	private transient String pw_;
+	private String email_;
+	private int authority_;
 	
 	public memberDto(){}
 	
 	public memberDto(String id, String name, String pw, String email, int authority) {
-		this.id = id;
-		this.name = name;
-		this.pw = pw;
-		this.email = email;
-		this.authority = authority;
+		this.id_ = id;
+		this.name_ = name;
+		this.pw_ = pw;
+		this.email_ = email;
+		this.authority_ = authority;
 	}
 
 	public String getId() {
-		return id;
+		return id_;
 	}
 	public void setId(String id) {
-		this.id = id;
+		this.id_ = id;
 	}
 	public String getName() {
-		return name;
+		return name_;
 	}
 	public void setName(String name) {
-		this.name = name;
+		this.name_ = name;
 	}
 	public String getPw() {
-		return pw;
+		return pw_;
 	}
 	public void setPw(String pw) {
-		this.pw = pw;
+		this.pw_ = pw;
 	}
 	public String getEmail() {
-		return email;
+		return email_;
 	}
 	public void setEmail(String email) {
-		this.email = email;
+		this.email_ = email;
 	}
 	public int getAuthority() {
-		return authority;
+		return authority_;
 	}
 	public void setAuthority(int authority) {
-		this.authority = authority;
+		this.authority_ = authority;
 	}
 
 	@Override
 	public String toString() {
-		return "memberDto [id=" + id + ", name=" + name + ", pw=" + pw + ", email=" + email + ", authority=" + authority
+		return "memberDto [id=" + id_ + ", name=" + name_ + ", pw=" + pw_ + ", email=" + email_ + ", authority=" + authority_
 				+ "]";
 	}
 }
